7fce8f045049892ddd758a16aee0c67c2d6ffc1a
Torbjørn Øvrebekk <torbjorn.ovrebekk@nordicsemi.no>
2020-10-16 16:50:05 +0200

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace LongLapseOrganizer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            string[] files = Directory.GetFiles(folderBrowserDialog1.SelectedPath, "*.NEF");
+            Console.WriteLine("Found " + files.Length + " files");
+        }
+    }
+}
