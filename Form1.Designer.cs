f189719c90451ed776df7bc7a0f0672865980e37
Torbjørn Øvrebekk <ovrebekk@gmail.com>
2020-10-24 18:31:12 +0200

    Simple export functionality working

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,12 +32,27 @@
             this.button1 = new System.Windows.Forms.Button();
             this.listView2 = new System.Windows.Forms.ListView();
             this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog2 = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnSaveList = new System.Windows.Forms.Button();
+            this.richTextBoxLog = new System.Windows.Forms.RichTextBox();
+            this.textBoxInputFolder = new System.Windows.Forms.TextBox();
+            this.textBoxOutputFolder = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.numericSkipNthFile = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxOutputFileName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericSkipNthFile)).BeginInit();
             this.SuspendLayout();
             // 
             // listView1
             // 
             this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(564, 49);
+            this.listView1.Location = new System.Drawing.Point(783, 62);
             this.listView1.Name = "listView1";
             this.listView1.Size = new System.Drawing.Size(290, 314);
             this.listView1.TabIndex = 0;
@@ -45,34 +60,177 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Location = new System.Drawing.Point(12, 114);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(125, 23);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Text = "Load input files";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // listView2
             // 
             this.listView2.HideSelection = false;
-            this.listView2.Location = new System.Drawing.Point(12, 49);
+            this.listView2.Location = new System.Drawing.Point(437, 62);
             this.listView2.Name = "listView2";
-            this.listView2.Size = new System.Drawing.Size(340, 267);
+            this.listView2.Size = new System.Drawing.Size(340, 254);
             this.listView2.TabIndex = 2;
             this.listView2.UseCompatibleStateImageBehavior = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 143);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(125, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Read Exif";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnSaveList
+            // 
+            this.btnSaveList.Location = new System.Drawing.Point(12, 255);
+            this.btnSaveList.Name = "btnSaveList";
+            this.btnSaveList.Size = new System.Drawing.Size(125, 23);
+            this.btnSaveList.TabIndex = 5;
+            this.btnSaveList.Text = "Save List";
+            this.btnSaveList.UseVisualStyleBackColor = true;
+            this.btnSaveList.Click += new System.EventHandler(this.btnSaveList_Click);
+            // 
+            // richTextBoxLog
+            // 
+            this.richTextBoxLog.Location = new System.Drawing.Point(12, 397);
+            this.richTextBoxLog.Name = "richTextBoxLog";
+            this.richTextBoxLog.Size = new System.Drawing.Size(1107, 99);
+            this.richTextBoxLog.TabIndex = 6;
+            this.richTextBoxLog.Text = "";
+            // 
+            // textBoxInputFolder
+            // 
+            this.textBoxInputFolder.Location = new System.Drawing.Point(12, 26);
+            this.textBoxInputFolder.Name = "textBoxInputFolder";
+            this.textBoxInputFolder.Size = new System.Drawing.Size(245, 20);
+            this.textBoxInputFolder.TabIndex = 7;
+            // 
+            // textBoxOutputFolder
+            // 
+            this.textBoxOutputFolder.Location = new System.Drawing.Point(12, 77);
+            this.textBoxOutputFolder.Name = "textBoxOutputFolder";
+            this.textBoxOutputFolder.Size = new System.Drawing.Size(245, 20);
+            this.textBoxOutputFolder.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Input folder";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Output folder";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(263, 23);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Browse";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(263, 74);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "Browse";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // numericSkipNthFile
+            // 
+            this.numericSkipNthFile.Location = new System.Drawing.Point(12, 214);
+            this.numericSkipNthFile.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericSkipNthFile.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericSkipNthFile.Name = "numericSkipNthFile";
+            this.numericSkipNthFile.Size = new System.Drawing.Size(77, 20);
+            this.numericSkipNthFile.TabIndex = 13;
+            this.numericSkipNthFile.Value = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 198);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Skip Nth frame";
+            // 
+            // textBoxOutputFileName
+            // 
+            this.textBoxOutputFileName.Location = new System.Drawing.Point(95, 214);
+            this.textBoxOutputFileName.Name = "textBoxOutputFileName";
+            this.textBoxOutputFileName.Size = new System.Drawing.Size(162, 20);
+            this.textBoxOutputFileName.TabIndex = 15;
+            this.textBoxOutputFileName.Text = "breen_int_test_";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(95, 198);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(84, 13);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Output file name";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1131, 508);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxOutputFileName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numericSkipNthFile);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxOutputFolder);
+            this.Controls.Add(this.textBoxInputFolder);
+            this.Controls.Add(this.richTextBoxLog);
+            this.Controls.Add(this.btnSaveList);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.listView2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.listView1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericSkipNthFile)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -82,6 +240,20 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ListView listView2;
         private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog2;
+        private System.Windows.Forms.Button btnSaveList;
+        private System.Windows.Forms.RichTextBox richTextBoxLog;
+        private System.Windows.Forms.TextBox textBoxInputFolder;
+        private System.Windows.Forms.TextBox textBoxOutputFolder;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.NumericUpDown numericSkipNthFile;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxOutputFileName;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -8,24 +8,117 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using MetadataExtractor;
+using MetadataExtractor.Formats.Exif;
+using MetadataExtractor.Formats.Iptc;
+using MetadataExtractor.Formats.Jpeg;
 
 namespace LongLapseOrganizer
 {
     public partial class Form1 : Form
     {
         private string[] localFiles;
+        private List<ImageRecord> imageRecordList;
 
         public Form1()
         {
             InitializeComponent();
+            localFiles = new string[0];
+            imageRecordList = new List<ImageRecord>();
+        }
+
+        public void logMessage(String message)
+        {
+            richTextBoxLog.Text = message + "\r\n" + richTextBoxLog.Text;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            folderBrowserDialog1.ShowDialog();
-            localFiles = Directory.GetFiles(folderBrowserDialog1.SelectedPath, "*.NEF");
-            Console.WriteLine("Found " + localFiles.Length + " .NEF files");
+            try
+            {
+                localFiles = System.IO.Directory.GetFiles(textBoxInputFolder.Text, "*.NEF");
+                logMessage(localFiles.Length + " .NEF files found");
+            }
+            catch(Exception exception)
+            {
+                logMessage(exception.ToString());
+            }
+        }
 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Exif button loaded
+            logMessage("Reading EXIF data: " + localFiles[0]);
+            int counter = 0;
+            imageRecordList = new List<ImageRecord>(localFiles.Length);
+            foreach(String file in localFiles)
+            {
+                ImageRecord imageRecord = new ImageRecord();
+                imageRecord.FileName = file;
+                var directories = ImageMetadataReader.ReadMetadata(file);
+                imageRecord.CaptureTime = directories[3].GetDateTime(ExifDirectoryBase.TagDateTimeOriginal);
+                imageRecordList.Add(imageRecord);
+                counter++;
+            }
+            imageRecordList.Sort();
+            logMessage("Done processing");
+        }
+
+        private void btnSaveList_Click(object sender, EventArgs e)
+        {
+            int outImageIndex = 1;
+            String outFolder = textBoxOutputFolder.Text;
+            if (outFolder != null && System.IO.Directory.Exists(outFolder))
+            {
+                for (int i = 0; i < imageRecordList.Count; i += (int)numericSkipNthFile.Value)
+                {
+                    System.Diagnostics.Process process = new System.Diagnostics.Process();
+                    System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+                    startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                    startInfo.FileName = "cmd.exe";
+                    startInfo.Arguments = "/C mklink \"" + outFolder + "\\" + textBoxOutputFileName.Text + outImageIndex.ToString("D4") + ".NEF\" \"" + imageRecordList[i].FileName + "\"";
+                    startInfo.Verb = "runas";
+                    outImageIndex++;
+                    process.StartInfo = startInfo;
+                    process.Start();
+                }
+                logMessage(outImageIndex.ToString() + " symbolic links created");
+            }
+            else logMessage("Invalid out folder");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            textBoxInputFolder.Text = folderBrowserDialog1.SelectedPath;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog2.ShowDialog();
+            textBoxOutputFolder.Text = folderBrowserDialog2.SelectedPath;
+        }
+    }
+    public class ImageRecord : IComparable<ImageRecord>
+    {
+        private String mFileName;
+        private DateTime mDateTime;
+
+        public DateTime CaptureTime
+        {
+            get { return mDateTime; }
+            set { mDateTime = value; }
+        }
+
+        public String FileName
+        {
+            get { return mFileName; }
+            set { mFileName = value; }
+        }
+
+        public int CompareTo(ImageRecord other)
+        {
+            return mDateTime.CompareTo(other.CaptureTime);
         }
     }
 }
